01aff270440a4bc412154e94b277d1509c0e37cb
nyergler <nyergler@committer.creativecommons.org>
2006-10-11 13:38:32 +0000

    Additional input for options which require more details.

--- mta/trunk/chooser.js
+++ mta/trunk/chooser.js
@@ -1,16 +1,14 @@
 
 var mta = new SciComMta();
+var getEl = YAHOO.ext.Element.get;
 
 getSettings = function() {
 
   // return an associative array of the current form settings
   result = new Array();
-  form_fields = ["field", "scaling"];
 
   for (key in SciComMta.prototype._field_order) {
      field_name = SciComMta.prototype._field_order[key];
-//alert(field_name);
-//alert(document.forms["mta_selector_form"][field_name].length);
      result[field_name] = document.forms["mta_selector_form"][field_name].value;
   }
 
@@ -34,3 +32,32 @@ updateMta = function() {
   document.getElementById('mta_link').innerHTML = mta.get_name();
 
 } // updateMta
+
+update_field = function(field_name) {
+
+    // get the field value
+    value = document.forms["mta_selector_form"][field_name].value;
+
+    // iterate over the options updating the visibility if necessary
+    var options = document.forms["mta_selector_form"][field_name].options;
+
+    for (i = 0; i < options.length; i++) {
+	var option = options.item(i);
+	var detail_element = getEl([field_name, option.value, "detail"].
+				   join("_"));
+
+	if (option.value == value) {
+	    // this value is selected; show it
+	    detail_element.setStyle("display", "block");
+	    detail_element.show(true);
+	} else {
+	    // make sure it's hidden...
+	    if (detail_element.isVisible()) {
+		detail_element.hide(true);
+		detail_element.setStyle("display", "none");
+	    }
+	}
+
+    } // for i...
+
+} // update_field
