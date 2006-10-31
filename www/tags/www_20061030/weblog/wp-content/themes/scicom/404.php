c2126f152d10ddf741287d35286ff2832004daf0
nyergler <nyergler@committer.creativecommons.org>
2006-10-31 17:35:18 +0000

    Committing pre-refresh tag operation.

---
+++ www/tags/www_20061030/weblog/wp-content/themes/scicom/404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+    <!-- text area -->
+    <div id="divtext">
+      <p id="startcontent">&nbsp;</p>
+
+		<h2 class="center">Error 404 - Not Found</h2>
+
+	</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
---
+++ www/tags/www_20061030/weblog/wp-content/themes/scicom/footer.php
@@ -0,0 +1,41 @@
+    <!-- footer -->
+    <div id="divfooter">
+
+	<p>
+		<?php bloginfo('name'); ?> is proudly powered by 
+		<a href="http://wordpress.org/">WordPress</a>
+		<br /><a href="feed:<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
+		and <a href="feed:<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
+	</p>
+		<?php wp_footer(); ?>
+
+      <p>
+        <a rel="license" href="http://creativecommons.org/licenses/by/2.5/"><img src="http://creativecommons.org/images/public/somerights20.png" width="88" height="31" border="0" alt="Creative Commons License" title="" /></a><br />
+        Except where otherwise <a href="http://creativecommons.org/policies#license">noted</a>, this site is<br />
+        licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/2.5/">Creative Commons License</a></p>
+    </div>
+
+<!--
+<rdf:RDF xmlns="http://web.resource.org/cc/"
+    xmlns:dc="http://purl.org/dc/elements/1.1/"
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+<Work rdf:about="">
+<license rdf:resource="http://creativecommons.org/licenses/by/2.5/" />
+</Work>
+<License rdf:about="http://creativecommons.org/licenses/by/2.5/">
+   <requires rdf:resource="http://web.resource.org/cc/Attribution" />
+   <permits rdf:resource="http://web.resource.org/cc/Reproduction" />
+   <permits rdf:resource="http://web.resource.org/cc/Distribution" />
+   <permits rdf:resource="http://web.resource.org/cc/DerivativeWorks" />
+   <requires rdf:resource="http://web.resource.org/cc/Notice" />
+</License>
+</rdf:RDF>
+-->
+
+  </div><!-- divpagearea -->
+
+</div><!-- divoutershell -->
+
+
+</body>
+</html>
