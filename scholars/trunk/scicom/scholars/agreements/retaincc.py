7ac3072842bd5d7647c67f58a052711732ec1c3e
nyergler <nyergler@committer.creativecommons.org>
2007-03-27 14:40:27 +0000

    Updates to joint SPARC agreement.

--- scholars/trunk/scicom/scholars/agreements/retaincc.py
+++ scholars/trunk/scicom/scholars/agreements/retaincc.py
@@ -10,8 +10,10 @@ from reportlab.lib import colors
 from support import *
 
 class RetainCC(object):
+    """SPARC/CC integrated addendum."""
 
-    NAME = "OpenAccess-Creative Commons"
+    NAME = "Access-Reuse"
+    VERSION = "1.0"
 
     def __call__(self, filename, manuscript="", journal="", author=[], 
                  publisher=""):
@@ -21,26 +23,16 @@ class RetainCC(object):
         while len(author) < 4:
             author.append("")
 
-        doc = SimpleDocTemplate(filename)
+        doc = getDocument(filename)
 
         Story = []
-
-        outer_style = styles["Normal"]
-        outer_style.spaceAfter = 8
-        outer_style.spaceBefore = 6
-        outer_style.leftIndent = inch * 0.12
-        outer_style.firstLineIndent = -1 * outer_style.leftIndent
-
-        inner_style = copy.deepcopy(outer_style)
-        inner_style.leftIndent = inch * 0.50
-        inner_style.firstLineIndent = -0.25 * inch
 
         # Section 1
         Story.append(
             Paragraph(
             """<seq id="main">. THIS ADDENDUM hereby modifies and supplements
             the attached Publication Agreement concerning the following
-            Article:""", outer_style)
+            Article:""", styles['outer_style'])
             )
 
         journal_info_table = Table([
@@ -55,7 +47,7 @@ class RetainCC(object):
         Story.append(
             Paragraph(
             """<seq id="main">. The parties to the Publication Agreement as
-            modified and supplemented by this Addendum are:""", outer_style)
+            modified and supplemented by this Addendum are:""", styles['outer_style'])
             )
 
         journal_info_table = Table([
@@ -73,40 +65,47 @@ class RetainCC(object):
         # Section 3
         Story.append(
             Paragraph(
-            """<seq id="main">. This Addendum and the Publication Agreement,
-            taken together, allocate all rights under copyright with respect
-            to all versions of the Article.  The parties agree that wherever
-            there is any conflict between this Addendum and the Publication
-            Agreement, the provisions of this Addendum are paramount and the
-            Publication Agreement shall be construed accordingly.""",
-            outer_style)
+            """<seq id="main">. This Addendum and the Publication Agreement, 
+taken together, allocate all rights under copyright with respect to all 
+versions of the Article.  The parties agree that wherever there is any 
+conflict between this Addendum and the Publication Agreement, the provisions 
+of this Addendum are paramount and the Publication Agreement shall be construed 
+accordingly.""",
+            styles['outer_style'])
             )
 
         # Section 4
         Story.append(
             Paragraph(
-            """<seq id="main">. <b>Author's Retention of Rights. </b>
-            Notwithstanding any terms in the Publication Agreement to the
-            contrary, AUTHOR and PUBLISHER agree that in addition to any rights
-            under copyright retained by Author in the Publication Agreement,
-            Author retains: (i) the rights to reproduce, to distribute, to
-            publicly perform, and to publicly display the Article in any medium
-            for non-commercial purposes; (ii) the right to prepare derivative
-            works from the Article; and (iii) the right to authorize others to
-            make any non-commercial use of the Article so long as Author receives
-            credit as author and the journal in which the Article has been
-            published is cited as the source of first publication of the Article.
-            For example, Author may make and distribute copies in the course of
-            teaching and research and may post the Article on personal or
-            institutional Web sites and in other open-access digital
-            repositories.""",
-            outer_style)
+            """<seq id="main">. <b>Author's Retention of Rights. </b>  
+Notwithstanding any terms in the Publication Agreement to the contrary, 
+AUTHOR and PUBLISHER agree that in addition to any rights under copyright 
+retained by Author in the Publication Agreement, Author retains: (i) the 
+rights to reproduce, to distribute, to publicly perform, and to publicly 
+display the Article in any medium for non-commercial purposes; (ii) the right 
+to prepare derivative works from the Article; and (iii) the right to authorize
+ others to make any non-commercial use of the Article so long as Author
+ receives credit as author and the journal in which the Article has been 
+published is cited as the source of first publication of the Article.  For 
+example, Author may make and distribute copies in the course of teaching and 
+research and may post the Article on personal or institutional Web sites and 
+in other open-access digital repositories.""",
+            styles['outer_style'])
             )
 
-        # Page Break
-        #Story.append(PageBreak())
-
         # Section 5
+        Story.append(
+            Paragraph(
+                """<seq id="main">. <b>Publisher's Additional Commitments.</b> 
+Publisher agrees to provide to Author within 14 days of first publication 
+and at no charge an electronic copy of the published Article in a format, 
+such as the Portable Document Format (.pdf), that preserves final page 
+layout, formatting, and content. No technical restriction, such as security 
+settings, will be imposed to prevent copying or printing of the document.""",
+                styles['outer_style'])
+            )
+                
+        # Section 6
         Story.append(
             Paragraph(
             """<seq id="main">. <b>Acknowledgment of Prior License Grants. </b>
@@ -119,7 +118,7 @@ class RetainCC(object):
             reflected in the Article as part of an agreement between Author
             or Author's employing institution and such funding entity, such as
             an agency of the United States government.""",
-            outer_style)
+            styles['outer_style'])
             )
 
         # Section 6
@@ -130,7 +129,7 @@ class RetainCC(object):
             However, if Publisher publishes the Article in the journal or in any
             other form without signing a copy of this Addendum, such publication
             manifests Publisher's assent to the terms of this Addendum.""",
-            outer_style)
+            styles['outer_style'])
             )
 
         # Signature
@@ -148,7 +147,27 @@ class RetainCC(object):
         journal_info_table.hAlign = 'LEFT'
         Story.append(journal_info_table)
 
-        doc.build(Story, onFirstPage=pageInfo, onLaterPages=pageInfo)
+        # Disclaimer
+        Story.append(
+            Paragraph(Disclaimer, styles['disclaimer'])
+            )
+
+        # SPARC box
+        Story.append(
+            BoxedText([
+                    [Paragraph(
+                        """SPARC (the Scholarly Publishing and Academic Resources Coalition) and the Association of Research Libraries (ARL) are not parties to this Addendum or to the Publication Agreement. SPARC and ARL make no warranty whatsoever in connection with the Article. SPARC and ARL will not be liable to Author or Publisher on any legal theory for any damages whatsoever, including without limitation any general, special, incidental or consequential damages arising in connection with this Addendum or the Publication Agreement.""",
+                        styles['boxed_paragraph'])],
+                    [Paragraph(
+                        """SPARC and ARL make no warranties regarding the information provided in this Addendum and disclaims liability for damages resulting from the use of this Addendum. This Addendum is provided on an "as-is" basis. No legal services are provided or intended to be provided in connection with this Addendum.""",
+                        styles['boxed_paragraph'])],
+                    ])
+            )
+
+        agreement = "%s %s" % (self.NAME, self.VERSION)
+        doc.build(Story, 
+                  onFirstPage=lambda x,y: pageInfo(agreement, x, y), 
+                  onLaterPages=lambda x,y: pageInfo(agreement, x, y))
 
 if __name__ == '__main__':
     retaincc("test.pdf", "Extraordinary Measures",
