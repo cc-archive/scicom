839ddbbc085b68bcfaf887ea0be354ca099cc211
nyergler <nyergler@committer.creativecommons.org>
2007-03-27 14:40:44 +0000

    Refactoring to share more code among agreement generators.

--- scholars/trunk/scicom/scholars/agreements/support.py
+++ scholars/trunk/scicom/scholars/agreements/support.py
@@ -6,18 +6,60 @@ from reportlab.rl_config import defaultPageSize
 from reportlab.lib.units import inch
 from reportlab.lib import colors
 
+import reportlab.lib.styles
+
 import os.path
 import scicom.scholars.static
 
-PAGE_HEIGHT=defaultPageSize[1]; PAGE_WIDTH=defaultPageSize[0]
+PAGE_HEIGHT = inch * 11
+PAGE_WIDTH = inch * 8.5
 styles = getSampleStyleSheet()
+
+styles.add(reportlab.lib.styles.ParagraphStyle(
+    'outer_style', styles['Normal']))
+styles['outer_style'].fontSize = 9
+styles['outer_style'].leading = 10
+
+styles['outer_style'].spaceAfter = 8
+styles['outer_style'].spaceBefore = 6
+styles['outer_style'].leftIndent = inch * 0.12
+styles['outer_style'].firstLineIndent = -1 * styles['outer_style'].leftIndent
+
+styles.add(reportlab.lib.styles.ParagraphStyle(
+        'inner_style', styles['outer_style']))
+styles['inner_style'].leftIndent = inch * 0.50
+styles['inner_style'].firstLineIndent = -0.25 * inch
+
+styles.add(reportlab.lib.styles.ParagraphStyle(
+    'disclaimer', styles['outer_style']))
+styles['disclaimer'].fontSize = 8
+styles['disclaimer'].leading = 9
+styles['disclaimer'].leftIndent = \
+    styles['disclaimer'].firstLineIndent = 0
+
+styles.add(reportlab.lib.styles.ParagraphStyle(
+    'boxed_paragraph', styles['disclaimer']))
+styles['boxed_paragraph'].fontName = 'Helvetica'
+styles['disclaimer'].fontSize = 7
+styles['disclaimer'].leading = 8
 
 Title = "ADDENDUM TO PUBLICATION AGREEMENT"
 Footer = "Model Author's Addendum to Publication Agreement 1.0"
-Disclaimer1 = "Neither Creative Commons nor Science Commons are parties to this agreement or provide legal advice."
-Disclaimer2 = "For more information, visit www.sciencecommons.org."
+Disclaimer = "<b>Neither Creative Commons nor Science Commons are parties to this agreement or provide legal advice.  Please visit www.sciencecommons.org for more information and specific disclaimers.</b>"
 
-def pageInfo (canvas, doc):
+def getDocument(filename):
+    """Returns a ReportLab Document with the appropriate margins set."""
+
+    doc = SimpleDocTemplate(filename)
+    doc.pagesize = (PAGE_WIDTH, PAGE_HEIGHT)
+    doc.leftMargin = doc.rightMargin = \
+        doc.bottomMargin = inch * 0.5
+
+    doc.topMargin = inch
+
+    return doc
+
+def pageInfo (agreement_name, canvas, doc):
     canvas.saveState()
 
     # draw the header
@@ -29,18 +71,26 @@ def pageInfo (canvas, doc):
     try:
         canvas.drawImage(os.path.abspath(os.path.join(
                     os.path.dirname(scicom.scholars.static.__file__), 'images',
-                    'scicom.gif')), inch, inch*.75,
+                    'scicom.gif')), inch * 0.5, inch * 0.6,
                          width=inch, height=inch*.28)
     except:
         pass
 
-    canvas.drawString(2.1 * inch, inch*.95, Footer)
-
-    canvas.setFont('Times-Bold', 8)
-    canvas.drawString(2.1 * inch, inch*.85, Disclaimer1)
-    canvas.drawString(2.1 * inch, inch*.75, Disclaimer2)
-    
+    canvas.drawString(0.6 * inch, inch*.5, agreement_name)
     canvas.restoreState()
+
+def BoxedText(contents):
+    """Returns a Table object wrapping the contents with a border around
+    the entire table."""
+
+    result = Table(contents)
+    result.setStyle(
+        TableStyle([
+                ('BOX', (0,0), (-1,-1), 0.25, colors.black)
+                ])
+        )
+
+    return result
 
 def fillInRow(value, label, width=inch*3):
 
