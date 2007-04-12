d05a923b3e1462da4397438f884455314a2dc88f
nyergler <nyergler@committer.creativecommons.org>
2007-04-12 18:30:31 +0000

    Stubbed out unit tests, abortive implementation of MTA wrapper classes.

--- mta/trunk/scicom.mta_/setup.py
+++ mta/trunk/scicom.mta_/setup.py
@@ -1,28 +1,52 @@
-# XXX license header here
+## Copyright (c) 2006-2007 Nathan R. Yergler, Creative Commons
+
+## Permission is hereby granted, free of charge, to any person obtaining
+## a copy of this software and associated documentation files (the "Software"),
+## to deal in the Software without restriction, including without limitation
+## the rights to use, copy, modify, merge, publish, distribute, sublicense,
+## and/or sell copies of the Software, and to permit persons to whom the
+## Software is furnished to do so, subject to the following conditions:
+
+## The above copyright notice and this permission notice shall be included in
+## all copies or substantial portions of the Software.
+
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+## FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+## DEALINGS IN THE SOFTWARE.
 
 from setuptools import setup
 
 setup(
     name = "scicom.mta",
-    version = "0.2",
-    packages = ['scicom.mta', 'scicom.mta_web'],
+    version = "0.3a1",
+    packages = ['scicom.mta'],
 
     # scripts and dependencies
     install_requires = ['setuptools',
-                        'CherryPy>=3.0beta2',
                         'SQLAlchemy',
-                        'simplejson',
-                        'Genshi'
                         ],
 
     include_package_data = True,
     namespace_packages = ['scicom'],
     zip_safe = False,
 
-    entry_points = { 'console_scripts':
-                     ['mta = scicom.mta_web.app:serve',
-                      ],
-                     },
+    # entry points for MTA processing
+    entry_points = """
+        [mta.classes]
+        scicom = scicom.mta.processors:loader
+        ubmta = scicom.mta.processors:loader
+        sla = scicom.mta.processors:loader
+
+        [mta.uri]
+        scicom = scicom.mta.processors:mta_uri
+        ubmta = scicom.mta.processors:ubmta_uri
+        sla = scicom.mta.processors:sla_uri
+
+    """,
 
     # author metadata
     author = 'Nathan R. Yergler',
