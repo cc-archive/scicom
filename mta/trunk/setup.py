f32da1150ae7d0e7c3ab4ef01e0116c077913e68
nyergler <nyergler@committer.creativecommons.org>
2007-04-12 18:31:06 +0000

    Reflect split into two packages.

--- mta/trunk/setup.py
+++ mta/trunk/setup.py
@@ -1,18 +1,37 @@
-# XXX license header here
+## Copyright (c) 2006-2007 Nathan R. Yergler, Creative Commons
+
+## Permission is hereby granted, free of charge, to any person obtaining
+## a copy of this software and associated documentation files (the "Software"),
+## to deal in the Software without restriction, including without limitation
+## the rights to use, copy, modify, merge, publish, distribute, sublicense,
+## and/or sell copies of the Software, and to permit persons to whom the
+## Software is furnished to do so, subject to the following conditions:
+
+## The above copyright notice and this permission notice shall be included in
+## all copies or substantial portions of the Software.
+
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+## FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+## DEALINGS IN THE SOFTWARE.
 
 from setuptools import setup
 
 setup(
-    name = "scicom.mta",
+    name = "scicom.mta_web",
     version = "0.2",
-    packages = ['scicom.mta', 'scicom.mta_web'],
+    packages = ['scicom.mta_web'],
 
     # scripts and dependencies
     install_requires = ['setuptools',
                         'CherryPy>=3.0beta2',
-                        'SQLAlchemy',
                         'simplejson',
-                        'Genshi'
+                        'Genshi',
+                        'zdaemon',
+                        'scicom.mta',
                         ],
 
     include_package_data = True,
@@ -20,7 +39,7 @@ setup(
     zip_safe = False,
 
     entry_points = { 'console_scripts':
-                     ['mta = scicom.mta_web.app:serve',
+                     ['server = scicom.mta_web.app:serve',
                       ],
                      },
 
