ba4c9e86f628d03b51f0aad67361e82104a43568
nyergler <nyergler@committer.creativecommons.org>
2007-05-07 22:37:27 +0000

    Updated user interface to use YUI for help windows; this solves the problem of storing them in separate files which don't receive the custom CSS from partners.

--- scholars/trunk/scicom/scholars/static/js/scholars.js
+++ scholars/trunk/scicom/scholars/static/js/scholars.js
@@ -1,3 +1,4 @@
+YAHOO.namespace("scholars");
 
 // validate the form prior to submission
 function validate() {
@@ -33,8 +34,38 @@ function validate() {
 
 } // validate
 
-// initially hide all help text
-$("document").ready(function() {
+// convenience function for creating help tool tips
+YAHOO.scholars.init_help_text = function(link_id, help_id) {
+
+    // make sure we have an array to hold the list of panels
+    if (!YAHOO.scholars.help_panels) {
+	YAHOO.scholars.help_panels = new Array();
+    }
+
+    // create the new panel and position it
+    var new_panel = new YAHOO.widget.Panel(help_id, 
+{close: false, visible: false, draggable: false, width:200,
+ effect:{effect:YAHOO.widget.ContainerEffect.FADE,duration:0.35} } ); 
+
+    var link_xy = YAHOO.util.Dom.getXY(link_id);
+    new_panel.cfg.setProperty('xy',[link_xy[0] + 15, link_xy[1]] );
+    var item_idx = YAHOO.scholars.help_panels.push(new_panel) - 1;
+
+    YAHOO.scholars.help_panels[item_idx].render();
+
+    // connect the event handlers
+    YAHOO.util.Event.addListener(link_id, "mouseover", 
+				 function(e) {YAHOO.scholars.help_panels[item_idx].show();});
+   YAHOO.util.Event.addListener(link_id, "mouseout", 
+				function(e) {window.setTimeout("YAHOO.scholars.help_panels[" + item_idx + "].hide();", 500);});
+
+    // disable clicks
+    YAHOO.util.Event.addListener(link_id, 'click', function(e){YAHOO.util.Event.preventDefault(e);});
+
+} // init_help_text
+
+// initialization for the client-side app
+YAHOO.scholars.init = function() {
 
    // HTML snippet for new author input boxes
    var AUTHOR_INPUT = '<label>&nbsp;</label><input class="additional" name="author" /><br/>';
@@ -47,6 +78,17 @@ $("document").ready(function() {
        return false;
        } );
 
+   // connect the validation handler to the button
    $("#generate").click(validate);
 
-}); // document.ready
+   // help roll-overs
+   // ****************
+   YAHOO.scholars.init_help_text('title', 'help_title');
+   YAHOO.scholars.init_help_text('journal', 'help_journal');
+   YAHOO.scholars.init_help_text('author', 'help_author');
+   YAHOO.scholars.init_help_text('publisher', 'help_publisher');
+   YAHOO.scholars.init_help_text('agreement', 'help_agreement');
+   
+} // init
+
+YAHOO.util.Event.onDOMReady(YAHOO.scholars.init); 
