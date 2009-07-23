192de576a300237e97f41c87f74660cde24f90e0
nkinkade <nkinkade@committer.creativecommons.org>
2009-07-23 15:54:43 +0000

    Add a links div to footer with links to /policies, /privacy and /terms.

--- www/branches/scwp_branch/wp-content/themes/scowp/footer.php
+++ www/branches/scwp_branch/wp-content/themes/scowp/footer.php
@@ -3,6 +3,15 @@
 </div>
 
 <div id="footer">
+
+<div id="footerLinks">
+       <ul>
+               <li><a href="http://creativecommons.org/policies">Policies</a></li>
+               <li><a href="http://creativecommons.org/privacy">Privacy</a></li>
+               <li><a href="http://creativecommons.org/terms">Terms of Use</a></li>
+       </ul>
+</div>
+
 <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">
 <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/88x31.png" />
 </a>
