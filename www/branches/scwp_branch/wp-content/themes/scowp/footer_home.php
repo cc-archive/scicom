8db465a7f2c015e8da70c78dfd76843a7af2ebfb
nkinkade <nkinkade@committer.creativecommons.org>
2009-07-23 16:08:21 +0000

    Moved footerLinks out of <p> and closed the <p> tag since it wasn't before.

--- www/branches/scwp_branch/wp-content/themes/scowp/footer_home.php
+++ www/branches/scwp_branch/wp-content/themes/scowp/footer_home.php
@@ -1,5 +1,4 @@
 <div id="divfooter">
-<p> 
 
 <div id="footerLinks">
        <ul>
@@ -9,6 +8,8 @@
        </ul>
 </div>
 
+<p> 
+
 <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">
 <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/88x31.png" />
 </a>
@@ -16,6 +17,8 @@
 <a href="http://creativecommons.org/policies#license">noted</a>, this
 work is<br/> 
  licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a>.
+
+</p>
 
 </div>
 <!-- divpagearea --></div>
